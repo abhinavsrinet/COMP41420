2838754c788e0f653d04f0ca3c4477e8708a0341
NIKHIL CHUDEKAR <n.chudekar.31@gmail.com>
2015-03-10 21:03:50 +0000

    Added unit test cases for addTextMove, moveKing, updateKingLocations

--- test_chessBoard.py
+++ test_chessBoard.py
@@ -20,3 +20,27 @@ class TestChessBoard(TestCase):
     def test_isFree(self):
         chess_obj = ChessBoard()
         self.assertEquals(chess_obj.isFree(3, 3), True)
+
+    def test__parseTextMove(self):
+        chess_obj = ChessBoard()
+        print chess_obj._parseTextMove('e2e4')
+        #chess_obj.moveKing((0, 4), (1, 4), True)
+
+    def test_addTextMove(self):
+        chess_obj = ChessBoard()
+        chess_obj.addTextMove('e2e4')
+        chess_obj.printBoard()
+
+    def test_moveKing(self):
+        chess_obj = ChessBoard()
+        self.assertEquals(chess_obj.moveKing((7, 4), (6, 4)), True)
+        self.assertEquals(chess_obj.moveKing((7, 4), (4, 4)), True)
+
+    def test_updateKingLocations(self):
+        chess_obj = ChessBoard()
+        chess_obj.addTextMove('e2e4')
+        chess_obj.addTextMove('f7f5')
+        chess_obj.printBoard()
+        chess_obj.addTextMove('e1e2')
+        # Gets the updated location of white king on the chess board.
+        print chess_obj._white_king_location
